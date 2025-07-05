7e9bac3215491449287527fb0bfec5cdcab2615a
Thang Pham <110776441+TPKAKA@users.noreply.github.com>
2025-07-05 07:47:46 +0700

    feat/ add javadoc

--- ThangPV32_JPL.L.A203/src/main/java/fa/training/utils/Constants.java
+++ ThangPV32_JPL.L.A203/src/main/java/fa/training/utils/Constants.java
@@ -1,5 +1,14 @@
 package fa.training.utils;
 
+/**
+ * Utility class for storing application-wide constants.
+ * <p>
+ * Currently defines the file name for customer data persistence.
+ * </p>
+ */
 public class Constants {
+    /**
+     * The file path used to store the list of customers.
+     */
     public static final String DATA_FILE = "customer.dat";
 }
